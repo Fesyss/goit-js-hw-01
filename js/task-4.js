aef5718db6b90a2fd8f68aef5c7bdf0276edc752
Rostislav <rudenkorostislav0@gmail.com>
2021-04-09 11:05:05 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -9,7 +9,7 @@ if (totalPrice > credits){
     console.log('Недостатньо коштів на рахунку!')
 } else if ( number === null){
     console.log('Скасовано користувачем!')
-} else if ( number === NaN ){
+} else if ( Number.isNaN(Number(number)) === NaN ){
     console.log('Ви ввели не число!')
 }else {
     console.log(`Ви купили ${number} дроїдів, на рахунку залишилося ${ credits - totalPrice} кредитів.`)
